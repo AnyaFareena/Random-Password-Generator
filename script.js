130b67f221e523469479dde8d0beb304be5fb19e
Anya <anyaanto1909@gmail.com>
2019-10-05 12:29:53 -0400

    added validation

--- script.js
+++ script.js
@@ -20,11 +20,18 @@ function generateRandomPassword() {
         if (upper){passwordCharSet += upperCase;}
         if (lower){passwordCharSet += lowerCase;}
         if (special){passwordCharSet += specialCase;}
-        //loop through the length to generate random number using Math function
-        for (var i=0;i< numberOfChar; i++) {
-            userPassword = userPassword + passwordCharSet.charAt(Math.floor(Math.random() * Math.floor(passwordCharSet.length - 1)));
-            document.getElementById("password").value=userPassword;
+        
+        //check is any of the options are selected if not display a message
+        console.log('passwordCharSet:' + passwordCharSet);
+        if (passwordCharSet != ""){
+            //loop through the length to generate random number using Math function
+            for (var i=0;i< numberOfChar; i++) {
+                userPassword = userPassword + passwordCharSet.charAt(Math.floor(Math.random() * Math.floor(passwordCharSet.length - 1)));
+                document.getElementById("password").value=userPassword;
         }
+    }
+        else {
+            window.alert("None of the options are selected to generate the Password");}
     }
 }
 
